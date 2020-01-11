290f3644826d4dd49e461504beedd06f12c1df66
Kadi Kraman <kadi.kraman@formidable.com>
2020-01-11 09:25:55 +0000

    Add boilerplate for the example app

---
+++ example/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
