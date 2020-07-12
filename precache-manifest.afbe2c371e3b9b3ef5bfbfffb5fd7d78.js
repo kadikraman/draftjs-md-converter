34bc4896d0f743320d77a5a267102fcf7e55b8c3
Kadi Kraman <hellokadi@gmail.com>
2020-07-12 13:22:34 +0100

    Updates

---
+++ precache-manifest.afbe2c371e3b9b3ef5bfbfffb5fd7d78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e7761c41d1bae079a0a97b47e6836c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69859144a71a7735c827",
+    "url": "/static/css/2.ae87030c.chunk.css"
+  },
+  {
+    "revision": "3d9483318813eb9cb92b",
+    "url": "/static/css/main.a4c99fb1.chunk.css"
+  },
+  {
+    "revision": "69859144a71a7735c827",
+    "url": "/static/js/2.5a8b344f.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/static/js/2.5a8b344f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d9483318813eb9cb92b",
+    "url": "/static/js/main.5e0284b4.chunk.js"
+  },
+  {
+    "revision": "fa0af807e30880911a9d",
+    "url": "/static/js/runtime-main.28faca3d.js"
+  }
+]);
