afeb87e880e3e8b2fba4474466dccb11f772d3e2
Nikolai Kristiansen <nikolaik@gmail.com>
2021-01-29 09:09:32 +0100

    Switch to esbuild from parcel-bundler
    Adds a ESM build in dist/index.esm.js
    
    Update .npmignore to exclude demo and .github dirs
    
    Remove most babel deps, keeping babel-eslint and @babel/core to keep eslint working
    
    Fix webpack dependency warning:
    Critical dependency: the request of a dependency is an expression
    
    Bump minimum required node version to latest supported

---
+++ build.js
@@ -0,0 +1,19 @@
+const esbuild = require('esbuild');
+const pkg = require('./package.json');
+
+const options = {
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  external: ['path']
+};
+(async () => {
+  await esbuild
+    .build({ ...options, outfile: pkg.main, format: 'cjs' })
+    .catch(() => process.exit(1));
+
+  await esbuild
+    .build({ ...options, outfile: pkg.module, format: 'esm' })
+    .catch(() => process.exit(1));
+})();
