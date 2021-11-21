3db489eac3475fd749b5bbd1571503b189c2e5ee
Kenan Yusuf <kenan.m.yusuf@gmail.com>
2021-11-21 17:41:46 +0000

    Fix extra styles type in mdToDraftjs declaration

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,23 @@
-declare module 'draftjs-md-converter' {
-  import { RawDraftContentState } from 'draft-js';
+declare module "draftjs-md-converter" {
+  import type { Syntax as ASTNodeTypes } from "@textlint/markdown-to-ast";
+  import type { RawDraftContentState } from "draft-js";
   export function draftjsToMd(
     raw: RawDraftContentState,
     extraMarkdownDict?: { [key: string]: string }
   ): string;
   export function mdToDraftjs(
     mdString: string,
-    extraStyles?: { [key: string]: string }
+    extraStyles?: {
+      inlineStyles?: {
+        [key in ASTNodeTypes]?: {
+          type: string;
+          symbol: string;
+          [key: string]: string;
+        };
+      };
+      blockStyles?: {
+        [key: string]: string;
+      };
+    }
   ): RawDraftContentState;
 }
